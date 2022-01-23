5ac241a057363183351e05c451dbba97fe1d8c58
RenzoEspinoza <37278863+RenzoEspinoza@users.noreply.github.com>
2022-01-23 00:50:39 -0500

    Updated knex config for production environment

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,17 @@
 require('dotenv').config();
 
 const PORT = process.env.PORT;
+const DATABASE_URL = process.env.DATABASE_URL;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
+const DB_ENV = process.env.DB_ENV;
 
 module.exports = {
-  PORT
+  PORT,
+  DATABASE_URL,
+  DB_USER,
+  DB_PASSWORD,
+  DB_NAME,
+  DB_ENV
 }
--- utils/knex.js
+++ utils/knex.js
@@ -1,15 +1,25 @@
+const config = require('./utils/config');
 const types = require('pg').types;
 const TIMESTAMPTZ_OID = 1184;
 const TIMESTAMP_OID = 1114;
 types.setTypeParser(TIMESTAMPTZ_OID, val => val);
 types.setTypeParser(TIMESTAMP_OID, val => val);
 
-module.exports = require('knex')({
-    client: 'pg',
-    connection: {
-        host : '127.0.0.1',
-        user : 'postgres',
-        password : 'postgresRenzo',
-        database : 'nba_title_belt_tracker'
+const knex = require('knex');
+const knexConfig = {
+    development : {
+        client : 'pg',
+        connection : {
+            host : '127.0.0.1',
+            user : config.DB_USER,
+            password : config.DB_PASSWORD,
+            database : config.DB_NAME
+        }
+    },
+    production : {
+        client : 'pg',
+        connection : config.DATABASE_URL
     }
-});
+};
+const environment = config.DB_ENV;
+module.exports = knex(knexConfig[environment])
